546e0c018147db33a230c0b9e8a5fb5fd17705b2
AdamP <adam.pusio@outlook.com>
2020-08-29 10:25:23 +0200

    prepareStatement example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class App
 {
-    public static void main( String[] args ) throws Exception {
-        DbCreator dbCreator = new DbCreator();
-        dbCreator.createAndLoadData();
+    public static void main( String[] args ) throws SQLException {
+//        DbCreator dbCreator = new DbCreator();
+//        dbCreator.createAndLoadData();
+        ConnectionManager connectionManager = new ConnectionManager();
+        Connection connection = connectionManager.getConnection();
+
+        connection.prepareStatement("SELECT REGION_NAME FROM REGIONS");
+
+
+        connection.close();
     }
 }
