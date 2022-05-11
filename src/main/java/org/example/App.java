c0ced02d5c74159d5a4f91cd817569ea43e2a530
Adam Pusio <adam.pusio@outlook.com>
2022-05-11 20:20:44 +0200

    JDBC - work in progress

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,56 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App
 {
-    public static void main( String[] args ) throws SQLException {
+    public static void main( String[] args ) throws Exception {
+//        DbCreator dbCreator = new DbCreator();
+//        dbCreator.createAndLoadData();
+
         ConnectionManager connectionManager = new ConnectionManager();
         Connection connection = connectionManager.getConnection();
+
+        PreparedStatement regionsStatement = connection.prepareStatement(
+                "SELECT region_id, region_name FROM regions WHERE region_id = ?");
+        regionsStatement.setInt(1, 1);
+        ResultSet resultSet = regionsStatement.executeQuery();
+        if(resultSet.next()) {
+            resultSet.getInt("region_id");
+            resultSet.getString("region_name");
+        }
+        regionsStatement.close();
+
+
         connection.close();
     }
+
+    public static void examples(Connection connection) throws SQLException {
+        //STATEMENT EXAMPLE
+//        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM *");
+//        boolean execute = preparedStatement.execute();
+//        preparedStatement.close();
+
+        //DIFFERENCE BETWEEN STATEMENT and PREPARED STATEMENT
+        Statement statement = connection.createStatement();
+//        int id = 2;
+        String id = "2";
+        statement.execute("SELECT * FROM * WHERE ID = " + id);
+        statement.close();
+
+        //PRZEKAZYWANIE PARAMETROW
+
+        //ZLE !!!!
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM * WHERE ID = " + id);
+        preparedStatement.execute();
+        preparedStatement.close();
+
+        //DOBRZE !!
+        PreparedStatement correctPreparedStatement = connection.prepareStatement("SELECT * FROM * WHERE ID = ? AND name = ?");
+        correctPreparedStatement.setString(1, id);
+        correctPreparedStatement.setString(2, "Adam");
+        correctPreparedStatement.execute();
+        correctPreparedStatement.close();
+    }
+
 }
